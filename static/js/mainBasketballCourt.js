c9e8d725d3a460c7f6d4b1efd6786362a720c20d
Griffin Esposito <espositogriffin@gmail.com>
2024-02-06 19:04:48 -0700

    added to container

--- static/js/mainBasketballCourt.js
+++ static/js/mainBasketballCourt.js
@@ -7,8 +7,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
+let container = document.getElementById( 'container' );
+container.appendChild( renderer.domElement );
 // Lighting
 const light = new THREE.HemisphereLight(0xffffff, 0x444444);
 light.position.set(1, 3, 1);
